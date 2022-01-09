beeb232859b165de48733a5c46bccc672272bd68
demonbeduin <team.sad.info@gmail.com>
2022-01-09 17:29:21 +0300

    changed 7

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
 	GOOGLE_CLIENT_ID: '1001207127382-p62ilq59cecq00jl20rfnrd5mk1l4g4f.apps.googleusercontent.com',
 	GOOGLE_CLIENT_SECRET: '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',
-	mongoURL: "mongodb+srv://emaily:emaily@emaily.stzuo.mongodb.net/Emaily?retryWrites=true&w=majority"
+	mongoURL: "mongodb+srv://emaily:emaily@emaily.stzuo.mongodb.net/Emaily?retryWrites=true&w=majority",
+	cookieKey: "abcdefjigklmnopqrstuvwxyz"
 };
