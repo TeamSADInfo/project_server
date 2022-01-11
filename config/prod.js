824a2c0ae38d9b2c31ea09271f4401d1a7cc4e56
demonbeduin <team.sad.info@gmail.com>
2022-01-11 20:53:11 +0300

    test prod

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+	GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURL: process.env.MONGO_URL,
+	cookieKey: process.env.COOKIE_KEY
+};
